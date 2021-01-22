6104447c03e7b3bb934cd9c501b4adbad0e3b149
alessandro-fior <alessandro.fior>
2021-01-22 11:33:11 +0100

    test

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -229,7 +229,7 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            open_Click_1();
+            //WindowsFormsApplication1.Form1.open_Click_1();
         }
     }
 }
